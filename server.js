784068099ba1aecd38a225156b99c74b3bc88bd0
Gabriel Brito <dosgabrielbrito@gmail.com>
2024-06-14 23:34:16 -0300

    Conexão com BD

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
-import http from 'http';
+import 'dotenv/config';
+import app from './src/app.js';
 
 const PORT = 3000;
 
-const rotas = {
-    '/': 'Curso de Express API',
-    '/livros': 'Entrei na rota /livros',
-    '/autores': 'Entrei na rota /autores'
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('Servidor escutando!');
 });
-
-
 
 
